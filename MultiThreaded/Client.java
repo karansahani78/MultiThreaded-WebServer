5f2ec5fbc732bffa92b6ccf4925b6589c78ccefe
Karan Sahani <119734907+karansahani78@users.noreply.github.com>
2024-04-15 01:59:38 +0530

    Update Client.java

--- MultiThreaded/Client.java
+++ MultiThreaded/Client.java
@@ -1,34 +1,33 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class Server {
-    
-    public void run() throws IOException, UnknownHostException{
-        int port = 8010;
-        ServerSocket socket = new ServerSocket(port);
-        socket.setSoTimeout(20000);
-        while(true){
-            System.out.println("Server is listening on port: "+port);
-            Socket acceptedConnection = socket.accept();
-            System.out.println("Connected to "+acceptedConnection.getRemoteSocketAddress());
-            PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream(), true);
-            BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
-            toClient.println("Hello World from the server");
-        }
+public class Client {
+
+    public void run() throws UnknownHostException, IOException{
+        int port = 8090;
+        InetAddress address = InetAddress.getByName("localhost");
+        Socket socket = new Socket(address, port);
+        PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        toSocket.println("Hello World from socket "+socket.getLocalSocketAddress());
+        String line = fromSocket.readLine();
+        toSocket.close();
+        fromSocket.close();
+        socket.close();
     }
 
-    public static void main(String[] args){
-        Server server = new Server();
+    public static void main(String[] args) {
+        Client singleThreadedWebServer_Client = new Client();
         try{
-            server.run();
-        }catch(Exception ex){
+            singleThreadedWebServer_Client.run();
+        } catch(Exception ex){
             ex.printStackTrace();
         }
     }
-
 }
